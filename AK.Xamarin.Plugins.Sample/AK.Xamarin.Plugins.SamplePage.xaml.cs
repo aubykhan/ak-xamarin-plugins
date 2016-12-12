4fbe793271a3324948c7d2c6accf1ef3a79b0117
aubykhan <aubykhan@gmail.com>
2016-12-12 19:06:36 +0500

    new project

---
+++ AK.Xamarin.Plugins.Sample/AK.Xamarin.Plugins.SamplePage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace AK.Xamarin.Plugins.Sample
+{
+	public partial class AK.Xamarin.Plugins.SamplePage : ContentPage
+	{
+		public AK.Xamarin.Plugins.SamplePage ()
+		{
+			InitializeComponent();
+}
+	}
+}
