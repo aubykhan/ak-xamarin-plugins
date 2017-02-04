9e1875855a4c97ffea5a6dd4a4d5b7ac1f70ff3f
aubykhan <aubykhan@gmail.com>
2017-02-04 21:37:57 +0500

    Add utility method for list view item selection handling.

---
+++ AK.Xamarin.Plugins/Utils/ListUtils.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AK.Xamarin.Utils
+{
+	/// <summary>
+	/// Utililty methods for lists.
+	/// </summary>
+	public static class ListUtils
+	{
+		/// <summary>
+		/// Handles list item selection to prevent highlighting by setting selected item as null. 
+		/// This should typically be called from listview item seleciton event handler.
+		/// </summary>
+		/// <param name="listView">List view.</param>
+		/// <param name="e">Selected item event arguments.</param>
+		/// <param name="useListItemAction">Action for handling selected item.</param>
+		public static void HandleListItemSelection(ListView listView, SelectedItemChangedEventArgs e, Action<object> useListItemAction)
+		{
+			if (e.SelectedItem == null)
+				return;
+
+			var item = e.SelectedItem;
+			listView.SelectedItem = null;
+
+			useListItemAction?.Invoke(item);
+		}
+	}
+}
